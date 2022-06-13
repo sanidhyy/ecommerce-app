d620b4d24107547363542a3d0122d4daf16fb364
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-06-13 22:13:03 +0530

    styles added

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
 import React from "react";
 
-const index = () => {
-  return <div>index</div>;
+const Home = () => {
+  return (
+    <>
+      HeroBanner
+      <div>
+        <h2>Best Selling Products</h2>
+        <p>Speakers of any variations</p>
+      </div>
+      <div>{["Product 1", "Product 2"].map((product) => product)}</div>
+      Footer
+    </>
+  );
 };
 
-export default index;
+export default Home;
