aa1ad29ed09bbb175de7a4e3994269f322258cc1
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-06-17 22:15:18 +0530

    dynamic date added

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
 
+// Footer
 const Footer = () => {
   return (
     <div className="footer-container">
-      <p>2022 E-commerce Store. All rights reserved.</p>
+      <p>{new Date().getFullYear()} E-commerce Store. All rights reserved.</p>
       <p className="icons">
         <AiFillInstagram />
         <AiOutlineTwitter />
